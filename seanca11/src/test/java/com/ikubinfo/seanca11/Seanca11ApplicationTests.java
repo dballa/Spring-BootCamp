09646927a3e649b3c0d97d199820163bd802ef8b
Guido Cota <guido.cota@ikubinfo.com>
2021-03-10 18:39:00 +0100

    init projec

---
+++ seanca11/src/test/java/com/ikubinfo/seanca11/Seanca11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikubinfo.seanca11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seanca11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
